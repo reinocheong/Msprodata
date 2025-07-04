51df0850d06dd91559fb675ebf3a78b4aa24f530
Reino <reinocheong@gmail.com>
2025-07-04 12:34:55 +0800

    Switch to build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install python dependencies
 pip install -r requirements.txt
 
-# Install weasyprint system dependencies
-apt-get update && apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libharfbuzz0b libfontconfig1
+apt-get update && apt-get install -y wkhtmltopdf
